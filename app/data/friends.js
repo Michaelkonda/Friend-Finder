4fad5fce61dd0c7f4cf8cd48d92158429d775301
Michaelkonda <48224171+Michaelkonda@users.noreply.github.com>
2019-07-20 17:41:10 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friendMatch = [
+  {
+    name: "Jim Carrey",
+    photo: "https://media1.giphy.com/media/l378fIHIADPMXGwww/giphy.gif?cid=790b76115cf6d98b666a346c51efafe1&rid=giphy.gif ",
+    scores: ["2", "2", "2", "2", "2"]
+  }, {
+    name: "Elvis Presley",
+    photo: "https://media.giphy.com/media/PVJ29hwRF1DsQ/giphy.gif",
+    scores: ["3", "3", "3", "3", "3"]
+  }, {
+    name: "van Damm",
+    photo: "https://media.giphy.com/media/DgfHSCfESA0oM/giphy.gif",
+    scores: ["1", "1", "1", "1", "1"]
+  }, {
+    name: "Dwayne The Rock Johnson",
+    photo: "https://media1.giphy.com/media/QLvRBqfLXCphu/giphy.gif?cid=790b76115cf6db8c37524c3177f76669&rid=giphy.gif",
+    scores: ["3", "2", "3", "1", "1"]
+  },{
+    name: "Arnold Schwarzenegger",
+    photo: "https://media.giphy.com/media/TxUPfG8fxcaNG/giphy.gif",
+    scores: ["2", "2", "1", "3", "3"]
+  },
+
+];
+
+
+
+module.exports = friendMatch;
